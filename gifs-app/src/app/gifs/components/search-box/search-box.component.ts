95f5abea845009c6d1605de817788e2825cead31
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-17 13:33:02 -0300

    agregando funcionalidad para search box component

--- gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
+    <!-- # es para una referencia del tag -->
+    <!-- los evento () tienen propiedades por asi decirlo keyup tiene keyup.enter para cuando una persona presiona la tecla enter -->
   `,
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  @ViewChild('txtTagInput') // sirve para obtener la referencia de un tag
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  /* searchTag(newTag: string): void {
+    console.log({ newTag });
+  } */
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+  }
+}

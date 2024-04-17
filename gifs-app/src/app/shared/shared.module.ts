c50150a9aebe3728c0b2218857a77982bb4ea101
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-17 13:23:55 -0300

    creando componentes

--- gifs-app/src/app/shared/shared.module.ts
+++ gifs-app/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({

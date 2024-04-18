eaa58fd1a6dca9afab740d97ad3d9ac68003d5e5
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-18 11:17:45 -0300

    agregando contact page & components sidebar

--- country-app/src/app/shared/shared.module.ts
+++ country-app/src/app/shared/shared.module.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent, HomePageComponent],
+  declarations: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+  ],
 })
 export class SharedModule {}

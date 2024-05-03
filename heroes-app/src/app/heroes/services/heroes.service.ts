fe1231194ef8194cc70640d2625758f786563c62
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-03 15:35:31 -0300

    agregando servicio de buscar hero por id

--- heroes-app/src/app/heroes/services/heroes.service.ts
+++ heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -14,5 +14,11 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpClient
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
   }
 }

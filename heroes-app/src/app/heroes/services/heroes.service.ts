cb2f0d63061f415819e0d3c08630ffbd5de683ac
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-06 10:18:19 -0300

    agregando servicios crud para hero

--- heroes-app/src/app/heroes/services/heroes.service.ts
+++ heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -25,6 +25,26 @@ export class HeroesService {
   getSuggestions(query: string): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(
       `${this.baseUrl}/heroes?superhero=${query}&_limit=6`
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Partial<Hero>): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+
+    return this.httpClient.patch<Hero>(
+      `${this.baseUrl}/heroes/${hero.id}`,
+      hero
+    );
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((err) => of(false)),
+      map((resp) => true)
     );
   }
 }

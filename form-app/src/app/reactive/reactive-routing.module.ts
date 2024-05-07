1c9ec84702ba5b5839f354224a0482ad53007088
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-07 08:55:33 -0300

    sistema de rutas

--- form-app/src/app/reactive/reactive-routing.module.ts
+++ form-app/src/app/reactive/reactive-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './page/basic-page/basic-page.component';
+import { DynamicPageComponent } from './page/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './page/switches-page/switches-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        component: BasicPageComponent,
+      },
+      {
+        path: 'dynamic',
+        component: DynamicPageComponent,
+      },
+      {
+        path: 'switches',
+        component: SwitchesPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'basic',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ReactiveRoutingModule { }
+export class ReactiveRoutingModule {}

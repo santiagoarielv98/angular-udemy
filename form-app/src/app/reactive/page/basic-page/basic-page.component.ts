54fdb5381080ec640a9bef460763b4576224236f
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-07 09:21:14 -0300

    modificando basic page

--- form-app/src/app/reactive/page/basic-page/basic-page.component.ts
+++ form-app/src/app/reactive/page/basic-page/basic-page.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    isStorage: new FormControl(0, [Validators.required, Validators.min(0)]),
+  });
+  // public myForm: FormGroup = this.fb.group({
+  //   name: [''],
+  //   price: [0],
+  //   isStorage: [0],
+  // });
+  constructor(public fb: FormBuilder) {}
 
+  onSave(): void {
+    if (this.myForm.invalid) return;
+
+    console.log(this.myForm.value);
+  }
 }
--- form-app/src/app/reactive/reactive.module.ts
+++ form-app/src/app/reactive/reactive.module.ts
@@ -5,17 +5,14 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './page/basic-page/basic-page.component';
 import { DynamicPageComponent } from './page/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './page/switches-page/switches-page.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     BasicPageComponent,
     DynamicPageComponent,
-    SwitchesPageComponent
+    SwitchesPageComponent,
   ],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}

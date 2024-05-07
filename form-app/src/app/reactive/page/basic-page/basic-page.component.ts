a1e9af094007ab7298feb47b2963c5789bc0afe1
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-07 09:39:05 -0300

    completando formularios basicos

--- form-app/src/app/reactive/page/basic-page/basic-page.component.ts
+++ form-app/src/app/reactive/page/basic-page/basic-page.component.ts
@@ -24,8 +24,32 @@ export class BasicPageComponent {
   constructor(public fb: FormBuilder) {}
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
+  }
+
+  isValidField(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+  getFieldError(field: string) {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Ete campo es requerido';
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+    return null;
   }
 }

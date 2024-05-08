f11943f3551a237a2c9663a6724d77a35743f7e7
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-08 10:26:45 -0300

    validaciones con funciones

--- form-app/src/app/auth/pages/register-page/register-page.component.ts
+++ form-app/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as CustomValidators from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(CustomValidators.firstNameAndLastnamePattern),
+      ],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(CustomValidators.emailPattern)],
+    ],
+    username: ['', [Validators.required, CustomValidators.cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {}
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
---
+++ form-app/src/app/shared/validators/validators.ts
@@ -0,0 +1,15 @@
+import { FormControl } from '@angular/forms';
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+export const cantBeStrider = (control: FormControl<string>) => {
+  const value = control.value.trim().toLowerCase();
+
+  if (value === 'strider') {
+    return {
+      noStrider: true,
+    };
+  }
+
+  return null;
+};

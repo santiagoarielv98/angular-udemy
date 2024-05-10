f080c0e532d3545b22e94537db8c2eab0646bdaa
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-10 15:04:41 -0300

    terminando con la seccion de ciclo de vida

--- lifecycle/src/app/products/components/price/price.component.ts
+++ lifecycle/src/app/products/components/price/price.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styles: ``,
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
+
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('price', 'ngOnInit');
+
+    this.interval$ = interval(1000).subscribe((value) =>
+      console.log('tick: ' + value)
+    );
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('price', 'ngOnChanges');
+    console.log({ changes });
+  }
+  ngOnDestroy(): void {
+    console.log('price', 'ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
+}

11c882a6616d0c14fb560490a791bdbd1bbe99de
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-13 17:09:51 -0300

    agregando pagina de marcadores

--- map-app/src/app/maps/pages/markers-page/markers-page.component.ts
+++ map-app/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef<HTMLDivElement>;
 
+  public map?: Map;
+  public currentZoom: number = 13;
+  public currentLngLat: LngLat = new LngLat(-74.5, 40);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+    this.map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.currentZoom,
+      minZoom: -2,
+      maxZoom: 18,
+    });
+
+    const marker = new Marker({
+      // color: "red"
+    })
+      .setLngLat(this.currentLngLat)
+      .addTo(this.map);
+  }
 }

53ced2da1d4cf21ad2a7685e387f8e7bfd013854
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-13 12:38:33 -0300

    configurando las rutas

--- map-app/src/app/app-routing.module.ts
+++ map-app/src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '/maps',
+    path: 'maps',
     loadChildren: () => import('./maps/maps.module').then((m) => m.MapsModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'maps',
   },
 ];
 

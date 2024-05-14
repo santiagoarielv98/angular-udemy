6417a5d6b0507ff577d2ba9d1d8c9f6f13596931
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-14 17:09:14 -0300

    seccion de signals terminada

--- directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ directive-signals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,13 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, OnDestroy, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
   templateUrl: './properties-page.component.html',
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy {
+  public counter = signal(10);
+
   public user = signal<User>({
     id: 1,
     email: 'george.bluth@reqres.in',
@@ -17,6 +19,22 @@ export class PropertiesPageComponent {
   public fullName = computed<string>(() => {
     return `${this.user()!.first_name} ${this.user()!.last_name}`;
   });
+
+  public userChangedEffect = effect(() => {
+    if (this.user().first_name) {
+      console.log(
+        'user changed',
+        `${this.user().first_name} - ${this.counter()}`
+      );
+    }
+  });
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+  increaseBy(value: number) {
+    this.counter.update((c) => c + value);
+  }
 
   onFieldUpdated(field: keyof User, value: string) {
     this.user.update((current) => {

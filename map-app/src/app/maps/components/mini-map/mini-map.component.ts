7af982e64c42fd5a54e16fb0a4c2646ad8c9a040
Santiago Ariel <santiagoarielv98@gmail.com>
2024-05-13 22:42:25 -0300

    terminando ejercicio

--- map-app/src/app/maps/components/mini-map/mini-map.component.ts
+++ map-app/src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styles: ``,
 })
-export class MiniMapComponent {}
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat?: [number, number];
+
+  @ViewChild('map')
+  public divMap?: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap?.nativeElement) throw 'Map Div not found';
+    if (!this.lngLat) throw "LngLat can't be null";
+
+    const map = new Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom,
+      interactive: false,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
+  }
+}
--- map-app/src/app/maps/pages/markers-page/markers-page.component.ts
+++ map-app/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,5 +1,15 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { LngLat, Map, Marker } from 'mapbox-gl';
+import { LngLat, Map, Marker, MarkerOptions } from 'mapbox-gl';
+
+interface MarkerAndColor {
+  color: string;
+  marker: Marker;
+}
+
+interface PlainMarker {
+  color: string;
+  lngLat: number[];
+}
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -8,6 +18,8 @@ import { LngLat, Map, Marker } from 'mapbox-gl';
 export class MarkersPageComponent implements AfterViewInit {
   @ViewChild('map')
   public divMap?: ElementRef<HTMLDivElement>;
+
+  public markers: MarkerAndColor[] = [];
 
   public map?: Map;
   public currentZoom: number = 13;
@@ -24,10 +36,71 @@ export class MarkersPageComponent implements AfterViewInit {
       maxZoom: 18,
     });
 
+    this.loadToLocalStorage();
+  }
+
+  createMarker() {
+    if (!this.map) return;
+
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+    const lngLat = this.map.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+
     const marker = new Marker({
-      // color: "red"
+      color,
+      draggable: true,
     })
-      .setLngLat(this.currentLngLat)
+      .setLngLat(lngLat)
       .addTo(this.map);
+
+    this.markers.push({
+      color,
+      marker,
+    });
+    this.saveToLocalStorage();
+
+    marker.on('dragend', () => {
+      this.saveToLocalStorage();
+    });
+  }
+
+  deleteMarker(index: number) {
+    this.markers[index].marker.remove();
+    this.markers.splice(index, 1);
+  }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map(
+      ({ color, marker }) => ({ color, lngLat: marker.getLngLat().toArray() })
+    );
+
+    localStorage.setItem('plainMarkers', JSON.stringify(plainMarkers));
+  }
+
+  loadToLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+
+    const plainMarkers: PlainMarker[] = JSON.parse(plainMarkersString);
+
+    plainMarkers.forEach(({ color, lngLat }) => {
+      const [lng, lat] = lngLat;
+      const coords = new LngLat(lng, lat);
+
+      this.addMarker(coords, color);
+    });
   }
 }
